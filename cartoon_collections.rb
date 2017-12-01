2b9c3dce42373a0383ec7723b9571f483177aedf
Bperez4197 <bryceperez31@gmail.com>
2017-12-01 08:16:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,5 +23,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find? {|cheese| cheese == "cheddar", "gouda", "camembert"}
+  array.find? {|cheese| cheese == "cheddar","gouda","camembert"}
 end
