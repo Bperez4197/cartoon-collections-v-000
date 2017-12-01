be399432c584a3801c4aebbdd790b8cdbbabdbf6
Bperez4197 <bryceperez31@gmail.com>
2017-12-01 07:48:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
-def roll_call_dwarves(array)
+def roll_call_dwarves(dwarves)
   i = 0
-  array.each_with_index {|name| puts "#{name}" }
+  dwarves.each_with_index {|name| puts "#{name}" }
   i += 1
 end
 
